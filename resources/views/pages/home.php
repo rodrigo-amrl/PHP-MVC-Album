406de49bbe3b5ab25c82594e8d7a08bf0ecea249
Rodrigo Amaral <rodrigo.amaral94@hotmail.com>
2023-04-21 12:29:11 -0300

    teste

--- resources/views/pages/home.php
+++ resources/views/pages/home.php
@@ -12,10 +12,10 @@ $albuns = [
         'nome' => "teste",
         'descricao_curta' => "Tes\zxfsfte",
         'imagem_inicial' => 'floresta.jpg',
-        'descricao' => 'Teste'
+        'descricao' => 'Te ste'
     ]
 ];
-?>
+?> 
 <section class="py-5 text-center container">
     <div class="row py-lg-5">
         <div class="col-lg-6 col-md-8 mx-auto">
